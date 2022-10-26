f08b7d208f992b3bcdb6298db9fe57127524fa22
Omar-ibarra <47434258+Omar-ibarra@users.noreply.github.com>
2022-10-26 12:41:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,11 @@ $arr_size = 1000000;
 
 $arr = array();
 $arr1 = array();
-$arr2 = array();
 
 
 for ($i=1;$i<=$arr_size;$i++) {
 $arr[] = $i;
 $arr1[] = $i;
-$arr2[] = $i;
 echo "created an array of $arr_size entries";
 }
 
